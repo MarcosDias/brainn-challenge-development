947c41626bed6c7f83a4539deeaab66b4d492c00
Marcos Dias <marcos.a.melo.dias@gmail.com>
2018-10-28 01:05:51 -0300

    Implementation of the page find user

---
+++ project/frontend/vue-project/src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <button class="btn">
+    {{label}}
+    <span class="icon-triangle" />
+  </button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: [ 'label' ]
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/variable';
+
+.btn {
+  display: flex;
+  align-items: center;
+  box-shadow: 1px 1px 1px #000000;
+  background: $black-color;
+  border: none;
+  color: $white-color;
+  font-size: 16px;
+  padding: .5em 2em;
+  border-radius: $size-radius;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  .icon-triangle {
+    margin-left: $size-radius;
+    border-top: $size-radius solid transparent;
+    border-left: $size-radius solid $white-color;
+    border-bottom: $size-radius solid transparent;
+  }
+}
+</style>
---
+++ project/frontend/vue-project/src/pages/FindUser.vue
@@ -0,0 +1,50 @@
+<template>
+  <main class="main">
+
+    <div class="main--box">
+      <span>https://github.com/</span>
+      <input class="main--input" type="text" placeholder="username">
+    </div>
+
+    <div class="main--btn">
+      <btn label="get repositories" />
+    </div>
+
+    <!-- <ProgressBarBox /> -->
+
+  </main>
+</template>
+
+<script>
+// import ProgressBarBox from '../components/ProgressBarBox'
+import Button from '../components/Button'
+
+export default {
+  name: 'FindUser',
+  components: { 'btn': Button }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/variable';
+
+.main {
+  height: 70vh;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+
+  &--btn {
+    margin-top: 10px;
+  }
+
+  &--input {
+    padding: 5px 10px;
+    border: 2px solid $black-color;
+    color: $black-color;
+    width: 200px;
+    border-radius: $size-radius;
+  }
+}
+</style>
