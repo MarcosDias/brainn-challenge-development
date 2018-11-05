39d672da235993dd2a43ddd866ff85c5c7b32ade
Marcos Dias <marcos.a.melo.dias@gmail.com>
2018-11-05 01:32:18 -0200

    Update router

--- project/frontend/vue-project/src/router.js
+++ project/frontend/vue-project/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Base from '@/pages/Base'
 import FindUser from '@/pages/FindUser'
+import GridUser from '@/pages/GridUser'
 
 Vue.use(Router)
 
@@ -9,11 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Base',
       component: Base,
       children: [{
         path: '',
         component: FindUser
+      }, {
+        path: ':username',
+        component: GridUser
       }]
     }
   ]
