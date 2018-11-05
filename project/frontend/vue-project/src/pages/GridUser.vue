eef171c230d33d10f14877c0103cedee9cbf053a
Marcos Dias <marcos.a.melo.dias@gmail.com>
2018-11-05 01:31:59 -0200

    Grid User page

---
+++ project/frontend/vue-project/src/pages/GridUser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="grid container">
+
+    <input class="grid__input input" type="search" placeholder="search by tag" v-model="search">
+
+    <SearchableTable class="grid__table" :data="listRepositories" :search="search" />
+
+  </div>
+</template>
+
+<script>
+import SearchableTable from '@/components/SearchableTable'
+
+import http from '@/services/http'
+
+export default {
+  name: 'GridUser',
+  components: { SearchableTable },
+  props: ['user'],
+  data: () => ({
+    search: ''
+  }),
+  created () {
+    this.listRepositories = http.getListRepositories()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/common';
+
+.grid {
+  padding-top: 8vh;
+
+  &__input {
+    background: url(https://static.tumblr.com/ftv85bp/MIXmud4tx/search-icon.png) no-repeat 9px center;
+    padding-left: 32px;
+  }
+
+  &__table {
+    margin-top: 5vh;
+  }
+}
+</style>
