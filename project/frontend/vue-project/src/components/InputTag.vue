cbd021bff6343127b1a34b81b87444f96cb32675
Marcos Dias <marcos.a.melo.dias@gmail.com>
2018-11-05 01:30:34 -0200

    Input Tag Component

---
+++ project/frontend/vue-project/src/components/InputTag.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="input-tag">
+    <Tag
+      v-for="(nameTag, index) in tags" :key="index"
+      :name="nameTag" :index="index"
+      @remove-tag="removeTag" />
+    <input
+      type="text" class="input-tag__input" :placeholder="placeholder"
+      @keyup.enter="addTag()" v-model="newTag">
+  </div>
+</template>
+
+<script>
+import Tag from './Tag'
+
+export default {
+  name: 'InputTag',
+  components: { Tag },
+  props: {
+    tags: {
+      default: []
+    },
+    placeholder: {
+      type: String
+    }
+  },
+  data: () => ({
+    newTag: null
+  }),
+  methods: {
+    addTag () {
+      if (!!this.newTag && !this.tags.includes(this.newTag)) {
+        this.tags.push(this.newTag)
+        this.newTag = null
+      }
+    },
+    removeTag (tagRemove) {
+      this.tags = this.tags.filter(tag => (tag !== tagRemove))
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/common';
+@import '../assets/style/variable';
+
+.input-tag {
+  @extend .input-style;
+  padding: 2px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  &__input {
+    border: none;
+    color: $black-color;
+    padding: 5px;
+  }
+}
+</style>
---
+++ project/frontend/vue-project/src/components/Tag.vue
@@ -0,0 +1,57 @@
+<template>
+  <span class="tag">
+    {{name}}
+    <a v-if="!readOnly" class="tag__close" href="#" @click.prevent="remove">x</a>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    readOnly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    remove () {
+      this.$emit('remove-tag', this.name)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/common';
+@import '../assets/style/variable';
+
+.tag {
+  @extend .input-style;
+  background-color: $black-color;
+  color: $white-color;
+  font-size: .9em;
+  margin-right: 4px;
+  margin-top: 2px;
+  margin-bottom: 2px;
+  padding: 4px;
+  font-size: .8em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: auto;
+
+  &__close {
+    text-decoration: none;
+    color: $white-color;
+    cursor: pointer;
+    font-weight: 700;
+    margin-left: 2px;
+  }
+}
+</style>
