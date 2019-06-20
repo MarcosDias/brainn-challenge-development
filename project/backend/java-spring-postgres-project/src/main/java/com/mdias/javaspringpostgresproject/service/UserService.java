beb30f9b016553d8fa17e7deede2ef4ab6df47a9
Marcos Dias <marcos.a.melo.dias@gmail.com>
2019-06-20 17:10:25 -0300

    Refatoring for better scope organization

--- project/backend/java-spring-postgres-project/src/main/java/com/mdias/javaspringpostgresproject/service/UserService.java
+++ project/backend/java-spring-postgres-project/src/main/java/com/mdias/javaspringpostgresproject/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mdias.javaspringpostgresproject.domain.User;
+import com.mdias.javaspringpostgresproject.exception.ResourceNotFound;
 import com.mdias.javaspringpostgresproject.repository.UserRepository;
 
 @Service
@@ -18,8 +19,22 @@ public class UserService {
 		return repository.findByName(username);
 	}
 
-	public User save(User user) {
+	public User create(String username) {
+		User user = User.builder().name(username).build();
 		return repository.save(user);
+	}
+
+	public User getUserLoaded(String username) throws ResourceNotFound {
+		Optional<User> optUser = this.findByName(username);
+		return optUser.orElseThrow(() -> new ResourceNotFound("User not found, sorry! :("));
+	}
+
+	public User getUserLoadedOrCreate(String username) {
+		Optional<User> optUser = this.findByName(username);
+		
+		if (optUser.isPresent()) return optUser.get();
+		
+		return this.create(username);
 	}
  
 }
--- project/backend/java-spring-postgres-project/src/test/java/com/mdias/javaspringpostgresproject/service/UserServiceTest.java
+++ project/backend/java-spring-postgres-project/src/test/java/com/mdias/javaspringpostgresproject/service/UserServiceTest.java
@@ -7,12 +7,14 @@ import static org.mockito.Mockito.when;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.mdias.javaspringpostgresproject.AbstractTest;
 import com.mdias.javaspringpostgresproject.domain.User;
+import com.mdias.javaspringpostgresproject.exception.ResourceNotFound;
 import com.mdias.javaspringpostgresproject.repository.UserRepository;
 
 public class UserServiceTest extends AbstractTest {
@@ -23,15 +25,20 @@ public class UserServiceTest extends AbstractTest {
 	@MockBean
 	private UserRepository repository;
 	
-
-	@Test
-	public void findByName() {
-		
+	@Before
+	public void setUp() {
 		when(repository.findByName(VALIDE_USER_GITHUB))
 			.thenReturn(Optional.of(createFakeUser()));
-		
+
+		when(repository.save(any(User.class)))
+			.thenReturn(createFakeUser());
+	}
+
+	@Test
+	public void findByName_test_ok() {
+
 		Optional<User> optUser = service.findByName(VALIDE_USER_GITHUB);
-		
+
 		assertThat(optUser)
 			.isNotNull()
 			.get()
@@ -41,15 +48,13 @@ public class UserServiceTest extends AbstractTest {
 	}
 
 	@Test
-	public void save() {
-		when(repository.save(any(User.class)))
-			.thenReturn(createFakeUser());
-		
+	public void create_test_ok() {
+
 		User fakeUser = createFakeUser();
 		fakeUser.setId(null);
-		
-		User userSaved = service.save(fakeUser);
-		
+
+		User userSaved = service.create(VALIDE_USER_GITHUB);
+
 		assertThat(userSaved)
 			.isNotNull()
 			.hasFieldOrPropertyWithValue("id", 1L)
@@ -57,12 +62,60 @@ public class UserServiceTest extends AbstractTest {
 			;
 		
 	}
+	
+	@Test
+	public void getUserLoaded_test_ok() throws ResourceNotFound {
+
+		User userLoaded = service.getUserLoaded(VALIDE_USER_GITHUB);
+
+		assertThat(userLoaded)
+			.isNotNull()
+			.hasFieldOrPropertyWithValue("id", 1L)
+			.hasFieldOrPropertyWithValue("name", VALIDE_USER_GITHUB)
+			;
+	}
+	
+	@Test(expected = ResourceNotFound.class)
+	public void getUserLoaded_test_error_resource_not_found() throws ResourceNotFound {
+		
+		service.getUserLoaded(INVALIDE_USER_GITHUB);
+	}
+	
+	@Test
+	public void getUserLoadedOrCreate_test_ok_user_already_created() {
+		User userLoaded = service.getUserLoadedOrCreate(VALIDE_USER_GITHUB);
+
+		assertThat(userLoaded)
+			.isNotNull()
+			.hasFieldOrPropertyWithValue("id", 1L)
+			.hasFieldOrPropertyWithValue("name", VALIDE_USER_GITHUB)
+			;
+	}
+	
+	@Test
+	public void getUserLoadedOrCreate_test_ok_create_new_user() {
+
+		when(repository.save(any(User.class)))
+			.thenReturn(createFakeUser(INVALIDE_USER_GITHUB));
+
+		User userCreated = service.getUserLoadedOrCreate(INVALIDE_USER_GITHUB);
+
+		assertThat(userCreated)
+			.isNotNull()
+			.hasFieldOrPropertyWithValue("id", 1L)
+			.hasFieldOrPropertyWithValue("name", INVALIDE_USER_GITHUB)
+			;
+	}
 
 	private User createFakeUser() {
+		return createFakeUser(VALIDE_USER_GITHUB);
+	}
+
+	private User createFakeUser(String userName) {
 		return User.builder()
 				.id(1L)
 				.createdDateTime(LocalDateTime.now())
-				.name(VALIDE_USER_GITHUB)
+				.name(userName)
 				.build();
 	}
 
